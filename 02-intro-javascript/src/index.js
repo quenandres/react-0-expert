986abe4ab121115e1e21628e7e7cb8a11f58bb35
Jhonatan Mejia <quenandres@gmail.com>
2021-09-03 20:47:10 -0500

    Explicación del scope y de let y const 13. Variables y constantes

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,1 +1,12 @@
-console.log("Hola mundo");
+const nombre = 'Fernando';
+const apellido = 'Herrera';
+
+let valorDado = 5;
+
+console.log(nombre, apellido, valorDado);
+
+if( true ) {
+    valorDado = 8;
+}
+
+console.log(valorDado);
