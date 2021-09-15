0eb9153a9d1ca2ac3c6ed1f907beaff97bf4f45b
Jhonatan Mejia <quenandres@gmail.com>
2021-09-14 23:32:37 -0500

    37. Impresión de variables en el HTML

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -4,9 +4,18 @@ import React from 'react';
 // Functional components
 
 const PrimeraApp = () => {
+
+    const saludo = {
+        nombre: 'Fernando',
+        edad: 34
+    };
+
+    const saludo2 = 'Hola';
+
     return (
         <>
-            <h1>Hola mundo</h1>
+            <h1>{ saludo2 }</h1>
+            {/* <pre>{ JSON.stringify(saludo, null, 3) }</pre> */}
             <p>dawwdaawd</p>
         </>
     );
