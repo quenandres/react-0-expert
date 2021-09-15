187ee19fae3b174b57ad14f8cc5d487d09dd1e5b
Jhonatan Mejia <quenandres@gmail.com>
2021-09-14 19:53:51 -0500

    36. Retornar elementos en el Componente - Fragment

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -1,9 +1,15 @@
 import React from 'react';
+//import React, { Fragment } from 'react';
 
 // Functional components
 
 const PrimeraApp = () => {
-    return <h1>Hola mundo</h1>;
+    return (
+        <>
+            <h1>Hola mundo</h1>
+            <p>dawwdaawd</p>
+        </>
+    );
 }
 
 export default PrimeraApp;
