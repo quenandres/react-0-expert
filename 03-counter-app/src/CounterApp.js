41544e51d87902c600b015c80c6fb9fbb73b6ef7
Jhonatan Mejia <quenandres@gmail.com>
2021-09-15 20:17:04 -0500

    43. useState - Hook

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({ value }) => {
     
+    // 
+    const [ counter, setCounter ] = useState(0); // Primer parametro nombre de la variable, segundo es una funcion que se coloca con set<nombre de la variable>
+    console.log(counter);
+
+    // handleAdd
     const handleAdd = (e) => {
-        console.log(e);
-        return 1;
+        //setCounter( counter + 1 );
+        setCounter( c => c + 1 );
     }
 
     return (
         <>
             <h1>CounterApp</h1>
-            <h2> { value } </h2>
+            <h2> { counter } </h2>
             <button onClick={ handleAdd }>+1</button>
         </>)
 }
