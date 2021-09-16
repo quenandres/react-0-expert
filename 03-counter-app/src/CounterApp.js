202fa8ed4fe8114d87fec3f915a75678572be528
Jhonatan Mejia <quenandres@gmail.com>
2021-09-15 20:23:06 -0500

    44. handleSubtract y handleReset

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -12,12 +12,24 @@ const CounterApp = ({ value }) => {
         //setCounter( counter + 1 );
         setCounter( c => c + 1 );
     }
+    
+    const handleReset = () => {
+        setCounter( value );
+    }
+    const handleSubstract = () => {
+        setCounter( counter - 1 );
+    }
+
+
+
 
     return (
         <>
             <h1>CounterApp</h1>
             <h2> { counter } </h2>
             <button onClick={ handleAdd }>+1</button>
+            <button onClick={ handleReset }>0</button>
+            <button onClick={ handleSubstract }>-1</button>
         </>)
 }
 
