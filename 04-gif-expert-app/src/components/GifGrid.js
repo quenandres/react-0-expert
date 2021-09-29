4d667da2b528158ef484d5ae9798af10f85f4aa4
Jhonatan Mejia <quenandres@gmail.com>
2021-09-28 20:17:06 -0500

    77. Helpers - getGifs

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { getGifs } from '../helpers/getGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ( { category } ) => {
@@ -6,22 +7,12 @@ export const GifGrid = ( { category } ) => {
     const [images, setImages] = useState([]);
 
     useEffect(() => {
-        getGifs();
-    }, []) // Segundo argumento vacio para que determine que no tiene dependencias
+        getGifs( category ).then(
+            imgs => setImages(imgs)
+        );
+    }, [ category ]) // Segundo argumento vacio para que determine que no tiene dependencias
 
-    const getGifs = async () => {
-        const url = 'https://api.giphy.com/v1/gifs/search?q=batman&limit=10&api_key=xJw4xi7Ze2WIz1WMAEkpY9Ppnc7NUkKF';
-        const resp = await fetch( url );
-        const { data } = await resp.json();
-        const gifs = data.map( img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-            }
-        })
-        setImages( gifs );
-    }
+    
 
     return (
         <>
