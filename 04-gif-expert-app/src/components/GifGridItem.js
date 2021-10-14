2b176a12b74dba8633d3e19c807a95472bf8e94d
Jhonatan Mejia <quenandres@gmail.com>
2021-10-13 21:38:52 -0500

    90. Pruebas del componente - GifGridItem

--- 04-gif-expert-app/src/components/GifGridItem.js
+++ 04-gif-expert-app/src/components/GifGridItem.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 export const GifGridItem = ( {title, url} ) => {
 
     return (
-        <div className="card animate__rubberBand">
+        <div className="card animate__animated animate__rubberBand">
             <h1>Test</h1>
             <img src={ url } alt={ title } />
             <p> { title } </p>
--- 04-gif-expert-app/src/tests/components/GifGridItem.test.js
+++ 04-gif-expert-app/src/tests/components/GifGridItem.test.js
@@ -7,10 +7,30 @@ describe('Pruebas de GifGridItem', () => {
 
     const title = 'Titulo';
     const url = 'https://localhost/algo.png';
+    const wrapper = shallow(<GifGridItem title={ title } url = { url }/>);
 
     test('Mostrar correctamente el gifGridItem', () => {
-        const wrapper = shallow(<GifGridItem title={ title } url = { url }/>);
         expect(wrapper).toMatchSnapshot();
     })
+
+    test('debe tener un parrafo con el titulo', () => {
+        const p = wrapper.find('p'); //Obtengo la referencia al parrafo
+        expect( p.text().trim() ).toBe( title );
+    });
+
+    test('debe de tener la imagen al url del props', () => {
+        const img = wrapper.find('img');
+        //console.log(img.props()); // Para no obtener el shallow, si no mostrar codigo html(), para obtener solo los atributos se usa props() = { src: 'https://localhost/algo.png', alt: 'Titulo' }
+        //Para obtener unicamente una propiedad, img.prop('src') = https://localhost/algo.png
+        expect( img.prop('src') ).toBe( url );
+        expect( img.prop('alt') ).toBe( title );
+    });
+
+    test('debe tener animate__rubberBand', () => {
+        const class_rubber = 'animate__rubberBand';
+        const div = wrapper.find('div');
+        
+        expect( div.props().className.includes(class_rubber) ).toBe( true );
+    });
     
 });
