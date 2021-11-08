ca793f5735393f44b9d656236d2a1aa374f9ab89
Jhonatan Mejia <quenandres@gmail.com>
2021-11-07 19:04:43 -0500

    94. Simular un submit del formulario

--- 04-gif-expert-app/src/components/AddCategory.js
+++ 04-gif-expert-app/src/components/AddCategory.js
@@ -7,8 +7,10 @@ export const AddCategory = ( {setCategories} ) => {
     const handleInputChange = (e) => {
         setInputValue( e.target.value );
     }
+
     const handleSubmit = (e) => {
         e.preventDefault();
+        console.log('handleSubmit', inputValue);
         if( inputValue.trim().length > 2 ){
             setCategories( cats => [inputValue, ...cats] );
             setInputValue('');
@@ -16,7 +18,7 @@ export const AddCategory = ( {setCategories} ) => {
     }
 
     return (        
-        <form onSubmit={handleSubmit} >
+        <form onSubmit={ handleSubmit } >
             <p> { inputValue } </p>
             <input 
                 type="text" 
--- 04-gif-expert-app/src/tests/components/AddCategory.test.js
+++ 04-gif-expert-app/src/tests/components/AddCategory.test.js
@@ -4,8 +4,12 @@ import { shallow } from 'enzyme';
 import { AddCategory } from '../../components/AddCategory';
 
 describe('Pruebas en AddCategory', () => {
-    const setCategories = () => {};
-    const wrapper = shallow( <AddCategory setCategories={ setCategories }/> );
+    const setCategories = jest.fn(); // Para tener manejar de saber si se llamo, con que argumento se hizo
+    let wrapper = shallow( <AddCategory setCategories={ setCategories }/> );
+    beforeEach(() => {
+        jest.clearAllMocks(); //Para que se limpien las simulaciones
+        wrapper = shallow( <AddCategory setCategories={ setCategories }/> );
+    });
 
     test('debe de mostrarse correctamente', () => {
         expect( wrapper ).toMatchSnapshot();
@@ -16,6 +20,15 @@ describe('Pruebas en AddCategory', () => {
         const value = 'Hola mundo';
         input.simulate('change', { target: { value } });
         expect(wrapper.find('p').text().trim()).toBe(value);
-
     });
+
+    test('no debe de postear la informacion con submit', () => {
+        wrapper.find('form').simulate('submit', { preventDefault(){} });        
+        expect( setCategories ).not.toHaveBeenCalled();
+    });
+
+    /*
+    1. Simular el input change
+    2. 
+    */
 })
